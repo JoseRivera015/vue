2ef35ffa1a0adff1139dc265cd04dec1c9a74eb3
JoseRivera015 <63668886+JoseRivera015@users.noreply.github.com>
2021-05-22 03:19:01 -0500

    update script.js

--- js/script.js
+++ js/script.js
@@ -97,7 +97,7 @@ app.component('app-navbar',{
   template: `
     <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark mb-2">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">{{ location }}</a>
+        <a class="navbar-brand" href="#">{{ location(this.nav[1].link) }}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -127,7 +127,7 @@ app.component('app-navbar',{
     }
   },
   computed: {
-    location(this.nav[1].link) {
+    location(link) {
       let prelink = '../'
       let stringToCut = '/vue/'
       let str =  window.location.pathname.slice(stringToCut.length)
