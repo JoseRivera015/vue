dd395046cbe4e22a2abd134634230f52984cff60
JoseRivera015 <63668886+JoseRivera015@users.noreply.github.com>
2021-05-23 04:56:24 -0500

    update script.js

--- js/script.js
+++ js/script.js
@@ -127,14 +127,14 @@ app.component('app-jumbotron',{
 app.component('app-navbar',{
   template: `
     <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark mb-2">
-      <div class="container-fluid align-items-center">
+      <div class="container-fluid">
         <a class="navbar-brand" href="#">{{ brand }}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li v-for="item in nav" class="nav-item d-flex align-items-center">
+            <li v-for="item in nav" class="nav-item">
               <i :class="item.icon" class="me-2"></i>
               <a class="nav-link" :href="location(item.link)">{{ item.text }}</a>
             </li>
