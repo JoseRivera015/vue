788fc5cfd7ad60b703c0ad58177b53786627eb5f
JoseRivera015 <63668886+JoseRivera015@users.noreply.github.com>
2021-06-02 03:53:57 -0500

    update script.js

--- js/script.js
+++ js/script.js
@@ -142,7 +142,7 @@ app.component('app-navbar',{
     <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark mb-2">
       <div class="container-fluid d-flex align-items-center">
         <a class="navbar-brand" href="#">
-         <img style="height:30px;" :src="brand" alt="JRM">
+         <img style="height:30px;" :src="location(brand)" alt="JRM">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
