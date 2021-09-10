059bbd88bff382e56408903f20ed1c782bb82441
John Doe <johndoe@example.com>
2021-09-10 15:14:19 -0500

    y

---
+++ posts/test/youtubefeed.js
@@ -0,0 +1,62 @@
+const rootComponent = {
+  data() {
+    return {
+      items: [
+        {
+          videoLink: '/watch?v=gmW2pe2NT4o',
+          videoThumbnailImg: 'https://i.ytimg.com/vi/gmW2pe2NT4o/mqdefault.jpg',
+          videoTitle: 'Inta Eyh - XZEEZ Remix (Nancy Ajram)',
+          timeSatus: '2:58',
+
+          channelName: 'VAVA Media',
+          videoViews: '15M de vistas',
+          releaseDate: 'hace 3 meses',
+
+          channelLink: '/c/VAVAMedia',
+          channelImg: 'https://yt3.ggpht.com/ytc/AKedOLSy22zfIa5zHtoUITOaoDej71oWSMUK77gazE8FMQ=s68-c-k-c0x00ffffff-no-rj'
+        },
+        {
+          videoLink: '/watch?v=gmW2pe2NT4o',
+          videoThumbnailImg: 'https://i.ytimg.com/vi/gmW2pe2NT4o/mqdefault.jpg',
+          videoTitle: 'Inta Eyh - XZEEZ Remix (Nancy Ajram)',
+          timeSatus: '2:58',
+
+          channelName: 'VAVA Media',
+          videoViews: '15M de vistas',
+          releaseDate: 'hace 3 meses',
+
+          channelLink: '/c/VAVAMedia',
+          channelImg: 'https://yt3.ggpht.com/ytc/AKedOLSy22zfIa5zHtoUITOaoDej71oWSMUK77gazE8FMQ=s68-c-k-c0x00ffffff-no-rj'
+        },
+        {
+          videoLink: '/watch?v=gmW2pe2NT4o',
+          videoThumbnailImg: 'https://i.ytimg.com/vi/gmW2pe2NT4o/mqdefault.jpg',
+          videoTitle: 'Inta Eyh - XZEEZ Remix (Nancy Ajram)',
+          timeSatus: '2:58',
+
+          channelName: 'VAVA Media',
+          videoViews: '15M de vistas',
+          releaseDate: 'hace 3 meses',
+
+          channelLink: '/c/VAVAMedia',
+          channelImg: 'https://yt3.ggpht.com/ytc/AKedOLSy22zfIa5zHtoUITOaoDej71oWSMUK77gazE8FMQ=s68-c-k-c0x00ffffff-no-rj'
+        },
+        {
+          videoLink: '/watch?v=gmW2pe2NT4o',
+          videoThumbnailImg: 'https://i.ytimg.com/vi/gmW2pe2NT4o/mqdefault.jpg',
+          videoTitle: 'Inta Eyh - XZEEZ Remix (Nancy Ajram)',
+          timeSatus: '2:58',
+
+          channelName: 'VAVA Media',
+          videoViews: '15M de vistas',
+          releaseDate: 'hace 3 meses',
+
+          channelLink: '/c/VAVAMedia',
+          channelImg: 'https://yt3.ggpht.com/ytc/AKedOLSy22zfIa5zHtoUITOaoDej71oWSMUK77gazE8FMQ=s68-c-k-c0x00ffffff-no-rj'
+        }
+      ]
+    }
+  }
+}
+const app = Vue.createApp(rootComponent)
+const vm = app.mount('#app')
