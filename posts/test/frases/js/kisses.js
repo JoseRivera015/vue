77c76727f5b95b9f6a2e3aeb5a44684c8fb050ef
John Doe <johndoe@example.com>
2021-10-18 18:44:13 -0500

    y

---
+++ posts/test/frases/js/kisses.js
@@ -0,0 +1,12 @@
+let kisses = [
+  {
+    phrase: "Guapo el que lo lea"
+  },
+  {
+    phrase: "Guapo el que lo lea 2"
+  },
+  {
+    phrase: "Guapo el que lo lea 3"
+  }
+]
+export {kisses}
---
+++ posts/test/frases/js/main.js
@@ -0,0 +1,31 @@
+const rootComponent = {
+  data() {
+    return {
+      kiss: [
+        {
+          phrase: "Guapo el que lo lea"
+        },
+        {
+          phrase: "Eres una hermosura :3"
+        },
+        {
+          phrase: "¡Que hoy te vaya bien!"
+        }
+      ],
+      count: 0
+    }
+  },
+  methods: {
+    getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    randomCount() {
+      this.count = this.getRandomInt(0, this.kiss.length -1)
+    }
+
+  }
+}
+const app = Vue.createApp(rootComponent)
+const vm = app.mount('#app')
